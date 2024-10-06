c41148fcf87d7874417150815751c0aebe1d4e0e
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-10-06 15:47:03 +0530

    header

---
+++ src/sections/mobile-nav.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet";
+
+export function MobileNav({
+  navItems,
+}: {
+  navItems: Array<{ name: string; href: string }>;
+}) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="h-6 w-6"
+          >
+            <line x1="3" x2="21" y1="6" y2="6" />
+            <line x1="3" x2="21" y1="12" y2="12" />
+            <line x1="3" x2="21" y1="18" y2="18" />
+          </svg>
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right">
+        <div className="flex flex-col space-y-4 mt-4">
+          {navItems.map((item) => (
+            <a
+              href={item.href}
+              className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
